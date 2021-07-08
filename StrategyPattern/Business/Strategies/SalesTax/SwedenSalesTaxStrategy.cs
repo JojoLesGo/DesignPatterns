9a236835181c51bedb3c3203c7d51459bdabea47
Jordan Keller <jik@majuredata.onmicrosoft.com>
2021-07-08 12:08:11 -0400

    Initial Commit - Strategy Pattern Implemented for regional sales tax

---
+++ StrategyPattern/Business/Strategies/SalesTax/SwedenSalesTaxStrategy.cs
@@ -0,0 +1,31 @@
+﻿using StrategyPattern.Business.Models;
+using System;
+
+namespace StrategyPattern.Business.Strategies.SalesTax
+{
+    class SwedenSalesTaxStrategy : ISalesTaxStrategy
+    {
+        public decimal GetTaxFor(Order order)
+        {
+            decimal totalTax = 0m;
+
+            foreach(var item in order.LineItems)
+            {
+                switch (item.Key.ItemType)
+                {
+                    case ItemType.Food:
+                        totalTax += (item.Key.Price * 0.06m) * item.Value;
+                        break;
+                    case ItemType.Literature:
+                        totalTax += (item.Key.Price * 0.08m) * item.Value;
+                        break;
+                    case ItemType.Service:
+                    case ItemType.Hardware:
+                        totalTax += (item.Key.Price * 0.25m) * item.Value;
+                        break;
+                }
+            }
+            return totalTax;
+        }
+    }
+}
